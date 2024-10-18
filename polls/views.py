7d73e3c41a8f22844940c46139d417a27bc0c81e
viktej <81267806+viktej@users.noreply.github.com>
2024-10-17 21:29:13 -0500

    update polls

--- polls/views.py
+++ polls/views.py
@@ -1,7 +1,6 @@
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import  HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
-from django.shortcuts import get_object_or_404, render
 from django.views import generic
 from .models import Choice, Question
 
