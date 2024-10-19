1e3dd41dd3da187cc4ee7d0b35a0ed223fae48ff
viktej <81267806+viktej@users.noreply.github.com>
2024-10-19 14:26:51 -0500

    tests update

--- polls/tests.py
+++ polls/tests.py
@@ -1,11 +1,5 @@
 # Create your tests here.
-
-import datetime
-
 from django.test import TestCase
-from django.utils import timezone
-
-from .models import Question
 
 
 class QuestionModelTests(TestCase):
